c2601161e45e83fc2e4d8d8247e1d0f8963525bd
=omar <o.wakeel@intouch.com>
2023-06-05 16:43:23 +0200

    add settings form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { SettingsFormComponent } from './pages/settings/components/settings-form/settings-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SettingsComponent
+    SettingsComponent,
+    SettingsFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/settings/components/settings-form/settings-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings-form',
+  templateUrl: './settings-form.component.html',
+  styleUrls: ['./settings-form.component.scss']
+})
+export class SettingsFormComponent {
+  settingsForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.settingsForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  submitForm() {
+    if (this.settingsForm.valid) {
+      console.log(this.settingsForm.value);
+      // Send form data to the server
+    }
+  }
+}
