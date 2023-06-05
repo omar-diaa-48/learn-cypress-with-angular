f64be4637ddefafaf5a14c24c611344b403a6ac9
=omar <o.wakeel@intouch.com>
2023-06-05 14:24:27 +0200

    add home and settings pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '*', redirectTo: '/' }
 ];
 
